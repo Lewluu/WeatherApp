6ec83071137697dd6f1177305ba60386f9d284c7
Lewluu <66873306+Lewluu@users.noreply.github.com>
2022-05-07 13:11:08 +0300

    jenkinsfile update

--- init.py
+++ init.py
@@ -5,7 +5,8 @@ import json
 api_key="90b3486670d48a81220917bdefd43c68"
 base_url="http://api.openweathermap.org/data/2.5/weather?"
 
-city_name=input("Enter city name: ")
+# city_name=input("Enter city name: ")
+city_name="Trusesti"
 
 complete_url=base_url+"appid="+api_key+"&q="+city_name
 
