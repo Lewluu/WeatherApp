07010ac43ad6fd2900d809d2b25f45bd1a264d8b
Lewluu <66873306+Lewluu@users.noreply.github.com>
2022-05-10 14:12:47 +0300

    client update

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+import json
 import socket
 import init_producer as producer
 import init_consumer as consumer
@@ -11,8 +12,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     # s.sendall(b"Hello, world")
 
     producer.initWeatherData()
-    data=pickle.dumps(consumer.getWeatherInfo())
-    s.sendall(data)
-
-    data = s.recv(1024)
-    print(f"Received {data!r}")
+    # data=pickle.dumps(consumer.getWeatherInfo())
+    data=consumer.getWeatherInfo()
+    for info in data:
+        info=json.dumps(info)
+        s.sendall(info.encode("utf-8"))
+        data = s.recv(1024)
+        print(f"Received {data!r}")
