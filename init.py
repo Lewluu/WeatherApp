728c67f5053d6234ad162d59a68fbad7dad8b136
Lewluu <66873306+Lewluu@users.noreply.github.com>
2022-05-10 15:29:09 +0300

    update

--- init.py
+++ init.py
@@ -1,11 +1,11 @@
 import yaml
 
 try:
-    with open('./python-job1_dev-producer/data_out.yaml') as file:
-        data=yaml.safe_load(file)
+    file=open('./python-job1_dev-producer/data_out.yaml')
+    data=yaml.safe_load(file)
 except:
-    with open('data_out.yaml') as file:
-        data=yaml.safe_load(file)
+    file=open('data_out.yaml')
+    data=yaml.safe_load(file)
 
 curr_temp=float(str(data[1]['temperature']).replace("['","").replace("']",""))
 last_hour_temp=0
