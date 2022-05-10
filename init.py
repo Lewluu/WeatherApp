1d53fbb440895b0017fa58c27b066074978d404f
Lewluu <66873306+Lewluu@users.noreply.github.com>
2022-05-10 15:19:04 +0300

    jenkisfile update

--- init.py
+++ init.py
@@ -8,5 +8,10 @@ except:
         data=yaml.safe_load(file)
 
 curr_temp=float(str(data[1]['temperature']).replace("['","").replace("']",""))
+last_hour_temp=0
+threshold_temp=10.0
+
+if abs(curr_temp-last_hour_temp)>threshold_temp:
+    print("The temperature has exceeded the threshold... sending notification!")
 
 print(curr_temp)
