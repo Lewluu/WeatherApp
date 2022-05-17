c3c1cf4eb8cfe225ee95cafcd740a915910078f8
Lewluu <66873306+Lewluu@users.noreply.github.com>
2022-05-17 16:59:14 +0300

    code update

--- init.py
+++ init.py
@@ -1,11 +1,10 @@
-from datetime import datetime
 import ast
 import sys
 
 city = sys.argv[1]
 path = "../data/weather_info_" + city + ".txt"
 
-#getting the last line in file
+#getting the first and last line from file
 data_file = list()
 try:
     with open(path, 'r') as file:
@@ -16,41 +15,28 @@ except Exception as e:
     exit()
 
 last_line = ast.literal_eval(data_file[len(data_file) - 1])
-if 'temperature' and 'humidity' and 'temperature-threshold' and 'humidity-threshold' in last_line:
+first_line = ast.literal_eval(data_file[0])
+
+#getting info about the last captions and the one hour ago captions
+if 'temperature' and 'humidity' and 'temperature-threshold' and 'humidity-threshold' in (first_line and last_line):
     curr_temperature = float(last_line['temperature'])
     curr_humidity = float(last_line['humidity'])
+    one_hour_ago_temperature = float(first_line['temperature'])
+    one_hour_ago_humidity = float(first_line['humidity'])
     threshold_temperature = last_line['temperature-threshold']
     threshold_humidity = last_line['humidity-threshold']
 else:
     print("Values not found in " + path)
     exit()
 
-#getting the time from one hour ago
-curr_time = datetime.now().strftime("%H:%M")
-one_hour_ago_time = str(int(curr_time[0:2]) - 1) + curr_time[2:6]
+#testing if the threshold values from temperature and/or humidity are exceeded
+diff_temperature = abs(round(curr_temperature - one_hour_ago_temperature, 2))
+diff_humidity = abs(round(curr_humidity - one_hour_ago_humidity, 2))
 
-#getting the temperature from exactly one hour ago
-for line in data_file:
-    #converting string to dict and remove the seconds
-    line = ast.literal_eval(line)
-    file_data_time = str(line['time'])[:-3]
-    if file_data_time == one_hour_ago_time:
-        print("Found data from one hour ago!")
+if diff_temperature >= threshold_temperature:
+    print("Values of threshold temperature exceeded ... sending mail notification ...")
 
-        file_data_temperature = float(line['temperature'])
-        file_data_humidity = float(line['humidity'])
-        diff_temperature = abs(round(curr_temperature - file_data_temperature, 2))
-        diff_humidity = abs(round(curr_humidity - file_data_humidity, 2))
-
-        print("Difference in temperature from one hour ago is: " + str(diff_temperature))
-        if diff_temperature > threshold_temperature:
-            print("Threshold temperature exceeded ... sending mail notification ...")
-
-        print("Difference in humidity from one hour ago is: " + str(diff_humidity))
-        if diff_humidity > threshold_humidity:
-            print("Threshold temperature exceeded ... sending mail notification ...")
-
-        break
-        
+if diff_humidity >= threshold_humidity:
+    print("Values of threshold humidity exceeded ... sending mail notification ...")
         
         
---
+++ readme.txt
@@ -0,0 +1,4 @@
+<-----------> Consumer process of the project <----------->
+
+- This process will only get the first and the last line from the output file, which contains the information about that last
+caption of temperature and humidity and the ones frome one hour ago
