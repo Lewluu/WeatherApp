3a7874d4ef3ae7e06dbbbb7d747e32e4ff4d7958
Lewluu <66873306+Lewluu@users.noreply.github.com>
2022-05-17 11:00:57 +0300

    code update

--- init.py
+++ init.py
@@ -1,29 +1,36 @@
 from datetime import datetime
 import ast
+import sys
 
-city = 'Botosani'
-
+city = sys.argv[1]
 path = "../data/weather_info_" + city + ".txt"
 
+#getting the last line in file
+data_file = list()
 try:
-    file = open(path,'r')
+    with open(path, 'r') as file:
+        for line in file: data_file.append(line)
+    file.close()
 except Exception as e:
     print(e)
     exit()
 
-#getting the current temperature, humidity from the last line of the output file
-print(len(file.readline()))
-for line in file: pass
-line = ast.literal_eval(line)
-curr_temperature = float(line['temperature'])
-curr_humidity = float(line['humidity'])
+last_line = ast.literal_eval(data_file[len(data_file) - 1])
+if 'temperature' and 'humidity' and 'temperature-threshold' and 'humidity-threshold' in last_line:
+    curr_temperature = float(last_line['temperature'])
+    curr_humidity = float(last_line['humidity'])
+    threshold_temperature = last_line['temperature-threshold']
+    threshold_humidity = last_line['humidity-threshold']
+else:
+    print("Values not found in " + path)
+    exit()
 
 #getting the time from one hour ago
 curr_time = datetime.now().strftime("%H:%M")
 one_hour_ago_time = str(int(curr_time[0:2]) - 1) + curr_time[2:6]
 
 #getting the temperature from exactly one hour ago
-for line in file:
+for line in data_file:
     #converting string to dict and remove the seconds
     line = ast.literal_eval(line)
     file_data_time = str(line['time'])[:-3]
