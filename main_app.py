3b85144041ae581dc46d4d3f7f9d01c1b024e8d5
Lewluu <66873306+Lewluu@users.noreply.github.com>
2022-05-23 14:21:59 +0300

    mail update

--- main_app.py
+++ main_app.py
@@ -1,7 +1,13 @@
 from src import Producer
 import os
 
+from src.mail import Mail
+
 Producer.init()
+
+Mail.init(Producer.getMailInfo()[0], Producer.getMailInfo()[1], Producer.getMailInfo()[2])
+Mail.addContent("random content")
+Mail.send()
 
 os.system("python src/consumer.py "
  + Producer.getCity() + " "
--- src/mail.py
+++ src/mail.py
@@ -1,3 +1,4 @@
+from email.mime.text import MIMEText
 import smtplib
 from src import Log
 
@@ -7,7 +8,7 @@ class Mail:
         
         mail_user = sender
         mail_password = password
-        to = [receiver]
+        to = receiver
         body = ""
 
         subject = 'Weather status update!'
@@ -17,19 +18,16 @@ class Mail:
         body += content + "\n"
 
     def send():
-        email_text = """\
-        From: %s
-        To: %s
-        Subject: %s
-
-        %s
-        """ % (mail_user, ", ".join(to), subject, body)
+        email_text = MIMEText(body)
+        email_text['Subject'] = subject
+        email_text['From'] = mail_user
+        email_text['To'] = to
 
         try:
             smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
             smtp_server.ehlo()
             smtp_server.login(mail_user, mail_password)
-            smtp_server.sendmail(mail_user, to, email_text)
+            smtp_server.sendmail(mail_user, to, email_text.as_string())
             smtp_server.close()
             print ("Email sent!")
             Log.addMesage("mail-module", "Email sent!")
